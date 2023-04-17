dd62c362f8e340bda37ec73ca7fc9840136df5da
s-013-12 <s-013-12@gumiforest.ae.krakow.pl>
2023-04-17 12:57:53 +0200

    introduce product catalog web controller

--- src/main/java/pl/jkanclerz/App.java
+++ src/main/java/pl/jkanclerz/App.java
@@ -3,10 +3,18 @@ package pl.jkanclerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.jkanclerz.productcatalog.HashMapProductStorage;
+import pl.jkanclerz.productcatalog.ProductCatalog;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ProductCatalog createNewProductCatalog() {
+        return new ProductCatalog(new HashMapProductStorage());
     }
 }
