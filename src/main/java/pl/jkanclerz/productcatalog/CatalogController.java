fee9e10875203dd4a9c1a51e1121bb2428e26f61
s-013-12 <s-013-12@gumiforest.ae.krakow.pl>
2023-04-17 13:00:41 +0200

    first spring integration test

--- src/main/java/pl/jkanclerz/productcatalog/CatalogController.java
+++ src/main/java/pl/jkanclerz/productcatalog/CatalogController.java
@@ -14,6 +14,7 @@ public class CatalogController {
     CatalogController(ProductCatalog productCatalog) {
         this.productCatalog = productCatalog;
     }
+
     @GetMapping("/api/products")
     List<Product> allProducts() {
         return productCatalog.allPublishedProducts();
