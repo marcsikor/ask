b01117884a8281528d250597bffcb7657f21f1d0
s-013-12 <s-013-12@gumiforest.ae.krakow.pl>
2023-04-24 13:03:08 +0200

    introduce e2e tests

--- src/main/java/pl/jkanclerz/App.java
+++ src/main/java/pl/jkanclerz/App.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import pl.jkanclerz.productcatalog.HashMapProductStorage;
 import pl.jkanclerz.productcatalog.ProductCatalog;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
@@ -16,6 +18,16 @@ public class App {
     @Bean
     ProductCatalog createNewProductCatalog() {
         ProductCatalog productCatalog = new ProductCatalog(new HashMapProductStorage());
+
+        String productId1 = productCatalog.addProduct("My ebok", "NIce one");
+        productCatalog.assignImage(productId1, "images/nice.jpeg");
+        productCatalog.changePrice(productId1, BigDecimal.TEN);
+        productCatalog.publishProduct(productId1);
+
+        String productId2 = productCatalog.addProduct("New Ebook", "NIce one");
+        productCatalog.assignImage(productId2, "images/nice.jpeg");
+        productCatalog.changePrice(productId2, BigDecimal.valueOf(20.20));
+        productCatalog.publishProduct(productId2);
 
         return productCatalog;
     }
